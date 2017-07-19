d840a73392eb0a63d721ede6a3d8400f655047f6
Erika Hughes <ehughes@knights.ucf.edu>
2017-07-19 23:45:07 +0000

    Tried to fix devise and failed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,14 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
   end
 
   def show
@@ -19,5 +22,11 @@ class GamesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :rating, :completed, :notes)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :games
+  attr_accessor :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
