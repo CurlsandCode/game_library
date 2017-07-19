ba3e1e40e21266baa36e2f17273ad9b43b36b183
Erika Hughes <ehughes@knights.ucf.edu>
2017-07-19 21:30:13 +0000

    Added routes and controllers for games and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  resources :users, only: [:show] do
+    resources :games
+  end
+
+  get '/community_games', to: 'games#community', as: 'community_games'
   root 'library#index'
 end
